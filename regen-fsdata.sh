f91addf4138a88b6185e54bfba6038200298e6dc
anton_artemov <anton.artyomov@monitoreal.com>
2025-03-14 03:33:31 +0200

    UI updates

--- regen-fsdata.sh
+++ regen-fsdata.sh
@@ -3,7 +3,7 @@
 if [ ! -f makefsdata ]; then
     # Doing this outside cmake as we don't want it cross-compiled but for host
     echo Compiling makefsdata
-    gcc -o build/makefsdata -I../../pico-sdk/lib/lwip/src/include/ -Iinclude -I. makefsdata/makefsdata.c
+    gcc -o build/makefsdata -Ibuild/pico-sdk/lib/lwip/src/include/ -Iinclude -I. makefsdata/makefsdata.c
 fi
 
 echo Regenerating fsdata.c
